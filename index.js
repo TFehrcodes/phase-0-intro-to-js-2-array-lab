88488bba02e51758214e3eb3a3de639ac97df190
Travis Fehr <travisefehr@gmail.com>
2022-05-02 16:44:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+  cats.push('Ralph');
+};
+
+function destructivelyPrependCat() {
+  cats.unshift('Bob');
+};
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+};
+
+function appendCat() {
+  const newArray = [...cats, 'Broom'];
+  return newArray;
+};
+
+function prependCat() {
+  const newArray = ['Arnold', ...cats];
+  return newArray;
+};
+
+function removeLastCat() {
+  const newArray = [...cats.slice(0, 2)];
+  return newArray;
+};
+
+function removeFirstCat() {
+  const newArray = [...cats.slice(1,3)];
+  return newArray;
+}
